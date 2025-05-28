57909bceb162625cdc964bebd52bd20e3bc62546
JackQuenta <jackalvaroqm@gmail.com>
2025-05-28 10:45:53 -0500

    Actualizacion Ejercicio 1 de estructuras repetitivas: Mejora de interfaz de usuario y validación de entrada

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,13 +1,19 @@
+/*1: Diseñar un algoritmo que imprima en la pantalla los primeros “n” múltiplos de 7 (positivos
+mayores que cero)*/
 #include <iostream>
 using namespace std;
 int main(){
 	int n, i, multiplos7;
-	cout<<Ingrese cuantos primeros multiplos de 7 desea ver: <<endl;
+	cout<<"Ingrese cuantos primeros multiplos de 7 desea ver: "<<endl;
 	cin>>n;
-	cout<<Los primeros <<n<<  multiplos de 7 son: <<endl;
-	for (i=1 ; i<=n ; i++){
-		multiplos7=7*i;
-		cout<<multiplos7<<endl;
-	}	
+	if (n<=0){
+		cout<<"Numero ingresado equivocado";	
+	}else{
+		cout<<"Los primeros "<<n<< " multiplos de 7 son: "<<endl;
+		for (i=1 ; i<=n ; i++){
+			multiplos7=7*i;
+			cout<<multiplos7<<endl;
+		}
+	}
 	return 0;
 }
