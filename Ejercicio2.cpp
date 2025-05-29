ddc59d71a35e1233cb826dc76828dbf28e949575
JackQuenta <jackalvaroqm@gmail.com>
2025-05-29 12:07:34 -0500

    Actualice el Ejercicio2.cpp de estructuras repetitivas: Agregue enunciado y se cambio el tipo de variable, ahora puede ingresar decimales

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,11 +1,13 @@
+/*2: Leer números por teclado hasta introducir uno negativo; en ese momento imprimir “Fin
+de la captura” y terminar.*/
 #include <iostream>
 using namespace std;
 int main(){
-	int n;
+	double numero;
 	cout<<"Ingrese cualquier numero, para terminar ingrese uno negativo: "<<endl;
 	do{
-	cin>>n;
-	}while (n>=0);	
+	cin>>numero;
+	}while (numero>=0);	
 	cout<<"Fin de la captura"<<endl;
 	return 0;
 }
