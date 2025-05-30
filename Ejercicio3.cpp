f2bcf286b191e95063b7fe46919a9890048405db
JackQuenta <jackalvaroqm@gmail.com>
2025-05-30 10:42:43 -0500

    Actualizacion del Ejercicio3.cpp : mejora de la interfaz permitiendo ver enunciado del ejercicio y comentando la variable que se uso

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,7 +1,8 @@
+/*3: Realizar un algoritmo que sume los N primeros números naturales usando bucles*/
 #include <iostream>
 using namespace std;
 int main(){
-	int n, i, suma;
+	int n, i, suma; //se uso suma como acumulador de resultados al sumar.
 	cout<<"Ingrese cantidad de los primeros numeros naturales que desea sumar: "<<endl;
 	cin>>n;
 	suma=0;
