534b2cab91c686cf8d29e02f60956684236be5b8
Carlos Gallardo <cgallardos@unjbg.edu.pe>
2025-06-09 11:52:05 -0500

    Mejorando presentacion y la estetica del codigo

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,19 +1,25 @@
-/*1: Diseñar un algoritmo que imprima en la pantalla los primeros “n” múltiplos de 7 (positivos
-mayores que cero)*/
 #include <iostream>
 using namespace std;
-int main(){
-	int n, i, multiplos7;
-	cout<<"Ingrese cuantos primeros multiplos de 7 desea ver: "<<endl;
-	cin>>n;
-	if (n<=0){
-		cout<<"Numero ingresado equivocado";	
-	}else{
-		cout<<"Los primeros "<<n<< " multiplos de 7 son: "<<endl;
-		for (i=1 ; i<=n ; i++){
-			multiplos7=7*i;
-			cout<<multiplos7<<endl;
-		}
-	}
-	return 0;
+
+int main() {
+    int n, i, multiplo;
+
+    cout << "=====================================" << endl;
+    cout << "   MULTIPLOS POSITIVOS DE 7          " << endl;
+    cout << "=====================================" << endl;
+    
+    cout << "Ingrese cuantos multiplos de 7 desea ver: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Numero ingresado invalido. Debe ser mayor que 0." << endl;
+    } else {
+        cout << "\nLos primeros " << n << " multiplos positivos de 7 son:" << endl;
+        for (i = 1; i <= n; i++) {
+            multiplo = 7 * i;
+            cout << multiplo << endl;
+        }
+    }
+
+    return 0;
 }
